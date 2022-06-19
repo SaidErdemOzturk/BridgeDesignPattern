167a5816ba9d7132d16e581752ec585657c9d266
SaidErdmOzturk <saiderdemozturk@gmail.com>
2022-06-19 05:06:02 +0300

    Add project files.

---
+++ BridgeCalisma/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace BridgeCalisma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Report report = new SalesReport();
+            report.raporCesidi = new Desktop();
+            report.Display();
+            
+            
+        }
+    }
+    interface IRaporCesidi
+    {
+        void Generate();
+    }
+    class Desktop : IRaporCesidi
+    {
+        public void Generate()
+        {
+            Console.WriteLine("desktop generate");
+        }
+    }
+    class Web : IRaporCesidi
+    {
+        public void Generate()
+        {
+            Console.WriteLine("Web Generate");
+        }
+    }
+
+    abstract class Report
+    {
+        protected IRaporCesidi _raporCesidi;
+        public IRaporCesidi raporCesidi
+        {
+            set
+            {
+                _raporCesidi = value;
+            }
+        }
+        public virtual void Display()
+        {
+            _raporCesidi.Generate();
+        }
+    }
+    class SalesReport : Report
+    {
+        public override void Display()
+        {
+            Console.WriteLine("satış raporu :");
+            _raporCesidi.Generate();
+        }
+    }
+    class PerformansReport : Report
+    {
+        public override void Display()
+        {
+            Console.WriteLine("performans raporu");
+            _raporCesidi.Generate();
+        }
+    }
+
+}
